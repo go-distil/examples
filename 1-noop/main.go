446a939141a6701ba6eeba7cbdabd2b7c1f97478
Michael Andersen <m.andersen@cs.berkeley.edu>
2016-05-05 22:22:50 -0700

    Add first example

---
+++ 1-noop/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "github.com/go-distil/distil"
+
+const BTrDB = "btrdb.bts.sensor.guru:4410"
+const Mongo = "btrdb.bts.sensor.guru"
+
+func main() {
+	// Get a handle to BTrDB and Mongo. go-distil is implemented as a library
+	// so there is no other distillate service to connect to
+	ds := distil.NewDISTIL(BTrDB, Mongo)
+
+	// Construct an instance of your distillate. If you had parameters for
+	// the distillate you would maybe have a custom constructor. You could
+	// also load the parameters from a config file, or some heuristic
+	// algorithm, which we will show in the next few examples
+	instance := &NopDistiller{}
+
+	// Now we add this distillate to the DISTIL engine. If you add multiple
+	// distillates, they will all get computed in parallel.
+	ds.RegisterDistillate(&distil.Registration{
+		// The class that implements your algorithm
+		Instance: instance,
+		// A unique name FOR THIS INSTANCE of the distillate. If you
+		// are autogenerating distillates, take care to never produce
+		// the same name here. We would normally use a UUID but opted
+		// for this so as to be more human friendly. If the program
+		// is restarted, this is how it knows where to pick up from.
+		UniqueName: "demo_noop_distillate",
+		// These are inputs to the distillate that will be loaded
+		// and presented to Process()
+		InputPaths: []string{"/upmu/a6_bus1/L1MAG"},
+		// These are the output paths for the distillate. They must
+		// also be strictly unique.
+		OutputPaths: []string{"/godistil/a6_bus1/L1MAG"},
+	})
+
+	//Now we tell the DISTIL library to keep all the registered distillates
+	//up to date. The program will not exit.
+	ds.StartEngine()
+}
