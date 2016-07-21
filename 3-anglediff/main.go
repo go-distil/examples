4757237c9c07de4203a0c8dcec0856a7f3cc526b
Omid <ardakanian@berkeley.edu>
2016-07-20 18:05:00 -0700

    Passed refPath as env var

--- 3-anglediff/main.go
+++ 3-anglediff/main.go
@@ -16,22 +16,53 @@ func main() {
 	// portable to different installations
 	ds := distil.NewDISTIL(distil.FromEnvVars())
 
+	refPath := os.Getenv("refpmupath")
+	trimRefPath := strings.TrimPrefix(refPath, "/upmu/")
+	sref := strings.Split("trimRefPath", "/")
+	refUtilityID := sref[0]
+
+
 	// Clearly you could have more advanced logic here, but this serves as
 	// a good example. Register a frequency distillate for L1ANG of
 	// every PMU that has a nonempty L1MAG stream.
-
-	// TODO: read the ref location for each stream
 	for _, path := range ds.ListExistingUpmuPaths() {
-		var pathref = path
-		trimPath := strings.TrimPrefix(path, "/upmu/")
-		instance := &AngleDifferenceDistiller{}
-		registration := &distil.Registration{
-			Instance:   instance,
-			UniqueName: "anglediff_" + strings.Replace(trimPath, "/", "_", -1),
-			InputPaths: []string{path + "/L1ANG", pathref + "/L1ANG"},
-			OutputPaths: []string{"/demo/" + trimPath + "/L1DIFF"},
+		if strings.Compare(path, refPath) != 0 {
+			trimPath := strings.TrimPrefix(path, "/upmu/")
+			s := strings.Split("trimPath", "/")
+			pmuUtilityID := s[0]
+			if strings.Compare(refUtilityID, pmuUtilityID) == 0 {
+				if ds.StreamFromPath(path + "/L1ANG") != nil {
+					instance := &AngleDifferenceDistiller{}
+					registration1 := &distil.Registration{
+						Instance:   instance,
+						UniqueName: "anglediff1_" + strings.Replace(trimRefPath, "/", "_", -1) + "_" + strings.Replace(trimPath, "/", "_", -1),
+						InputPaths: []string{path + "/L1ANG", refPath + "/L1ANG"},
+						OutputPaths: []string{"/demo/" + trimPath + "/L1DIFF"},
+					}
+					ds.RegisterDistillate(registration1)
+				}
+				if ds.StreamFromPath(path + "/L2ANG") != nil {
+					instance := &AngleDifferenceDistiller{}
+					registration2 := &distil.Registration{
+						Instance:   instance,
+						UniqueName: "anglediff2_" + strings.Replace(trimRefPath, "/", "_", -1) + "_" + strings.Replace(trimPath, "/", "_", -1),
+						InputPaths: []string{path + "/L2ANG", refPath + "/L2ANG"},
+						OutputPaths: []string{"/demo/" + trimPath + "/L2DIFF"},
+					}
+					ds.RegisterDistillate(registration2)
+				}
+				if ds.StreamFromPath(path + "/L3ANG") != nil {
+					instance := &AngleDifferenceDistiller{}
+					registration3 := &distil.Registration{
+						Instance:   instance,
+						UniqueName: "anglediff3_" + strings.Replace(trimRefPath, "/", "_", -1) + "_" + strings.Replace(trimPath, "/", "_", -1),
+						InputPaths: []string{path + "/L3ANG", refPath + "/L3ANG"},
+						OutputPaths: []string{"/demo/" + trimPath + "/L3DIFF"},
+					}
+					ds.RegisterDistillate(registration3)
+				}
+			}
 		}
-		ds.RegisterDistillate(registration)
 	}
 
 	ds.StartEngine()
